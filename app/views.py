cbd3d4184b989bcbef7089d0b9f874f6f5a5a794
ffff <ffff@localhost.local>
2014-08-12 22:24:52 -0400

    little tweaks etc

--- app/views.py
+++ app/views.py
@@ -3,44 +3,64 @@ from flask import g, session, request, url_for, flash
 from flask import redirect, render_template, jsonify
 from app import app
 
-class Collection:
 
+# models
+class Collection:
 	def __init__(self, title, songs):
 		self.title = title
 		self.songs = songs
 
 class Song:
-
 	def __init__(self, title, filename):
 		self.title = title
 		self.filename = filename
 
 
-songs = [
-	Song('there are no paths paths are made by walking', 'there-are-no-paths.mp3'), 
-	Song('champs des possibles', 'champs-des-possibles.mp3')
-	]
-
+# data
 collections = [
-	Collection('The way you float', songs)
-	]
+	Collection('Though way you float through space & time evaporates just as quickly', [
+		Song('there are no paths paths are made by walking', 'there-are-no-paths.mp3'), 
+		Song('champs des possibles', 'champs-des-possibles.mp3'),
+		Song('goal', 'goal.mp3')
+		]
+	)
+]
 
 
+# views
 @app.route('/')
 def index():
 	return render_template('index.html')
 
 @app.route('/looptest')
 def looptest():
+	all_song_filenames = get_all_filenames_from(collections)
+	print all_song_filenames
+	
+	return render_template(
+		'looptest.html', 
+		collections=collections, 
+		all_song_filenames=all_song_filenames)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# functions
+def get_all_filenames_from(collections):
 	all_song_filenames = []
 	for collection in collections:
 		all_song_filenames.extend(
 			[song.filename for song in collection.songs]
 		)
-
-	print all_song_filenames
-
-	return render_template(
-		'looptest.html', 
-		collections=collections, 
-		all_song_filenames=all_song_filenames)
+	return all_song_filenames
